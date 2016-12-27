018425100604ffc574d680ad631dd0c727c68c39
Shashank Singh <cse.shashanksingh@gmail.com>
2016-12-27 07:11:04 +0530

    HTML Parser Helper Methods Finished, Stripped out useless HTML code from DB

---
+++ resources/views/results/includes/navbar.blade.php
@@ -0,0 +1,3 @@
+<div class="navbar">
+    ABC
+</div>
---
+++ resources/views/results/show-single-result.blade.php
@@ -0,0 +1,14 @@
+@extends('results.layouts.master')
+
+@section('title')
+
+@endsection
+
+@section('content')
+
+    @include('results.includes.navbar')
+
+
+    {{--{!! $resultHTML !!}--}}
+
+@endsection
