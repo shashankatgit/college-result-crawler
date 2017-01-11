8e6f123f3ac9c035f7b954db391ebffc89aa9315
Shashank Singh <cse.shashanksingh@gmail.com>
2017-01-11 15:17:29 +0530

    Added database folder

---
+++ database/migrations/2016_12_08_135524_create_results_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('results', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('rollno',12);
+            $table->char('name',30);
+            $table->char('session',10);
+            $table->char('semester',1);
+            $table->float('percentage');
+            $table->text('result_html');
+
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('results');
+    }
+}
